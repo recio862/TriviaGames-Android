4853860c50054d292fc5b2e8e30812d82837a41f
recio862 <recio862@utexas.edu>
2014-12-30 21:04:44 -0600

    connecting main to login

--- Trivia/src/com/example/trivia/MainActivity.java
+++ Trivia/src/com/example/trivia/MainActivity.java
@@ -11,14 +11,11 @@ import android.view.MenuItem;
 public class MainActivity extends Activity {
 
 	SharedPreferences mPrefs;
+	boolean mSoundOn;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		
-		//Parse Code
-	/*	Parse.initialize(this, "ybMbNsW5K7M3tWC0hq5d2JJyiDDJfDW65eGRcYRc", "ny76yoFFCO2ACumEzDDzOqHs40udxmyaJkjHG5eo");
-		ParseObject.registerSubclass(Game.class);
-*/
 		
 		mPrefs = getSharedPreferences("ttt_prefs", MODE_PRIVATE);
 	
@@ -39,18 +36,15 @@ public class MainActivity extends Activity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
+    	getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
+    	int id = item.getItemId();
+        
+    	if (id == R.id.action_settings) {
             return true;
         }
         return super.onOptionsItemSelected(item);
